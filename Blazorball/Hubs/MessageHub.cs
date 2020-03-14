01cf54cedd216e0d1cbc87de4630771a1f04498d
Kyle <sdbagel@gmail.com>
2020-03-14 11:35:46 -0700

    Get room code

--- Blazorball/Hubs/MessageHub.cs
+++ Blazorball/Hubs/MessageHub.cs
@@ -9,9 +9,25 @@ namespace Blazorball.Hubs
 {
     public class MessageHub : Hub
     {
+        private static readonly Dictionary<string, int> hosts = new Dictionary<string, int>();
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task Register()
+        {
+            var currentId = Context.ConnectionId;
+            if (!hosts.ContainsKey(currentId))
+            {
+                // Generate room code
+                int code = new Random().Next(10000, 100000);
+                // maintain a lookup of connectionId-to-username
+                hosts.Add(currentId, code);
+                // re-use existing message for now
+                await Clients.Caller.SendAsync("SetRoomID", code);
+            }
         }
     }
 }
