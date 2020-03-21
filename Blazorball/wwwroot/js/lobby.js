70727e7b3ec20212da7e8afcb6dce9ade9ab0abf
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-03-21 14:16:15 -0700

    Add matterjs & related game code
    Added matterjs. Added code and css to display the default example canvas on game start.

---
+++ Blazorball/wwwroot/js/lobby.js
@@ -0,0 +1,38 @@
+﻿var canvas;
+
+window.gamestart = () => {
+    canvas = document.getElementById("gameCanvas");
+
+    // module aliases
+    var Engine = Matter.Engine,
+        Render = Matter.Render,
+        World = Matter.World,
+        Bodies = Matter.Bodies;
+
+    // create an engine
+    var engine = Engine.create();
+
+    // create a renderer
+    var render = Render.create({
+        canvas: canvas,
+        engine: engine,
+        options: {
+            height: window.innerHeight,
+            width: window.innerWidth
+        }
+    });
+
+    // create two boxes and a ground
+    var boxA = Bodies.rectangle(400, 200, 80, 80);
+    var boxB = Bodies.rectangle(450, 50, 80, 80);
+    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+    // add all of the bodies to the world
+    World.add(engine.world, [boxA, boxB, ground]);
+
+    // run the engine
+    Engine.run(engine);
+
+    // run the renderer
+    Render.run(render);
+}
