3ce89cb2d5e019511175562261890d41235aa464
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-03-28 17:31:12 -0700

    Game & client scaffolding updates
    Game now has a bounding box. Client UI will disappear when game starts. Added temporary scoring/timer thingy at the top of the game.

--- Blazorball/wwwroot/js/lobby.js
+++ Blazorball/wwwroot/js/lobby.js
@@ -22,13 +22,22 @@ window.gamestart = () => {
         }
     });
 
-    // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    // World options
+    engine.world.gravity.y = 0.2;
 
-    // add all of the bodies to the world
-    World.add(engine.world, [boxA, boxB, ground]);
+    // Create bounding box
+    var top = Bodies.rectangle(window.innerWidth / 2, 0, window.innerWidth, 1, { isStatic: true });
+    var bottom = Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 1, { isStatic: true });
+    var left = Bodies.rectangle(0, window.innerHeight / 2, 1, window.innerHeight, { isStatic: true });
+    var right = Bodies.rectangle(window.innerWidth, window.innerHeight / 2, 1, window.innerHeight, { isStatic: true });
+    World.add(engine.world, [top, bottom, left, right]);
+
+    // create two boxes
+    var boxA = Bodies.rectangle(800, 200, 80, 80);
+    var boxB = Bodies.rectangle(450, 50, 80, 80);
+
+    // add bodies to the world
+    World.add(engine.world, [boxA, boxB]);
 
     // run the engine
     Engine.run(engine);
