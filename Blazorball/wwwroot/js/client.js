fd1201feea5b20c923f56f41475bd48ec96e0c36
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-04-12 17:50:58 -0700

    Draw client UI

--- Blazorball/wwwroot/js/client.js
+++ Blazorball/wwwroot/js/client.js
@@ -5,7 +5,7 @@
 var clientCanvas;
 window.initcontrols = () => {
     clientCanvas = document.getElementById("controlsCanvas");
-    var maxSize = Math.min(window.innerHeight, window.innerWidth);
+    var maxSize = Math.min(window.innerHeight, window.innerWidth, 700);
     clientCanvas.width = maxSize;
     clientCanvas.height = maxSize;
 }
